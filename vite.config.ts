617ae0bcf021438c3d49f707dd3b21f1bd48ae18
Wasi_Khalid <aman.wasi01@gmail.com>
2024-05-14 17:13:12 +0500

    proxy added

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+	server: {
+		proxy: {
+			'/auth': {
+				target: 'https://jinjigo-server.onrender.com',
+				changeOrigin: true,
+			},
+		},
+	},
 	plugins: [react()],
 	resolve: {
 		alias: {
