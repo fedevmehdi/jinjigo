165d92c66c860dd339a7a0b8bbe593df9b0e0e5c
Wasi_Khalid <aman.wasi01@gmail.com>
2024-05-21 01:46:10 +0500

    login updated

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -41,11 +41,11 @@ export default function LoginPage() {
 		console.error("Google login failed:", response);
 	};
 
-	useEffect(() => {
-		if (userInfo?.token && !loading) {
-			navigate("/");
-		}
-	}, [userInfo]);
+	// useEffect(() => {
+	// 	if (userInfo?.token && !loading) {
+	// 		navigate("/");
+	// 	}
+	// }, [userInfo]);
 
 	return (
 		<div>
